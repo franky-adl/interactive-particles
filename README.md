0f96559031adc4e7af17f6397debe22be9d1dc87
Hung Chun Fai <arkon.digital.hcf@gmail.com>
2023-06-06 18:50:58 +0900

    copied the most basic setup from the reference github repo to make the particles work

---
+++ README.md
@@ -0,0 +1,8 @@
+## Interactive Particles
+
+Demo rebuilt with reference from https://github.com/brunoimbrizi/interactive-particles
+
+## How to run locally
+
+1. npm i
+2. make dev
---
+++ src/index.js
@@ -0,0 +1,110 @@
+// ThreeJS and Third-party deps
+import * as THREE from "three"
+import * as dat from 'dat.gui'
+import Stats from "three/examples/jsm/libs/stats.module"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+
+// Core boilerplate code deps
+import { createCamera, createRenderer, runApp } from "./core-utils"
+
+// Other deps
+import Particles from './Particles';
+import Img from './assets/sample-03.png'
+
+global.THREE = THREE
+// previously this feature is .legacyMode = false, see https://www.donmccurdy.com/2020/06/17/color-management-in-threejs/
+// turning this on has the benefit of doing certain automatic conversions (for hexadecimal and CSS colors from sRGB to linear-sRGB)
+THREE.ColorManagement.enabled = true
+
+/**************************************************
+ * 0. Tweakable parameters for the scene
+ *************************************************/
+const params = {
+  // general scene params
+}
+
+
+/**************************************************
+ * 1. Initialize core threejs components
+ *************************************************/
+// Create the scene
+let scene = new THREE.Scene()
+
+// Create the renderer via 'createRenderer',
+// 1st param receives additional WebGLRenderer properties
+// 2nd param receives a custom callback to further configure the renderer
+let renderer = createRenderer({ antialias: true }, (_renderer) => {
+  // best practice: ensure output colorspace is in sRGB, see Color Management documentation:
+  // https://threejs.org/docs/#manual/en/introduction/Color-management
+  _renderer.outputEncoding = THREE.sRGBEncoding
+})
+
+// Create the camera
+// Pass in fov, near, far and camera position respectively
+let camera = createCamera(50, 1, 10000, { x: 0, y: 0, z: 300 })
+
+/**************************************************
+ * 2. Build your scene in this threejs app
+ * This app object needs to consist of at least the async initScene() function (it is async so the animate function can wait for initScene() to finish before being called)
+ * initScene() is called after a basic threejs environment has been set up, you can add objects/lighting to you scene in initScene()
+ * if your app needs to animate things(i.e. not static), include a updateScene(interval, elapsed) function in the app as well
+ *************************************************/
+let app = {
+  async initScene() {
+    // OrbitControls
+    this.controls = new OrbitControls(camera, renderer.domElement)
+    this.controls.enableDamping = true
+
+    scene.background = new THREE.Color(0x222222)
+
+    this.particles = new Particles(this)
+		scene.add(this.particles.container)
+    this.particles.init(Img)
+
+    // GUI controls
+    const gui = new dat.GUI()
+
+    // Stats - show fps
+    this.stats1 = new Stats()
+    this.stats1.showPanel(0) // Panel 0 = fps
+    this.stats1.domElement.style.cssText = "position:absolute;top:0px;left:0px;"
+    // this.container is the parent DOM element of the threejs canvas element
+    this.container.appendChild(this.stats1.domElement)
+  },
+  // load a texture for the floor
+  // returns a promise so the caller can await on this function
+  loadTexture(mshStdFloor) {
+    return new Promise((resolve, reject) => {
+      var loader = new THREE.TextureLoader()
+      loader.load(Tile, function (texture) {
+        texture.wrapS = THREE.RepeatWrapping
+        texture.wrapT = THREE.RepeatWrapping
+        texture.repeat.set(40, 40)
+        mshStdFloor.material.map = texture
+        resolve()
+      }, undefined, function (error) {
+        console.log(error)
+        reject(error)
+      })
+    })
+  },
+  // @param {number} interval - time elapsed between 2 frames
+  // @param {number} elapsed - total time elapsed since app start
+  updateScene(interval, elapsed) {
+    this.controls.update()
+    this.stats1.update()
+
+    // rotate the torus
+    this.particles.update(interval)
+  }
+}
+
+/**************************************************
+ * 3. Run the app
+ * 'runApp' will do most of the boilerplate setup code for you:
+ * e.g. HTML container, window resize listener, mouse move/touch listener for shader uniforms, THREE.Clock() for animation
+ * Executing this line puts everything together and runs the app
+ * ps. if you don't use custom shaders, pass undefined to the 'uniforms'(2nd-last) param
+ * ps. if you don't use post-processing, pass undefined to the 'composer'(last) param
+ *************************************************/
+runApp(app, scene, renderer, camera, true, undefined, undefined)
